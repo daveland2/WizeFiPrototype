679df55d18fcab37d683f8c0a184e8529586f141
Dave Eland <dave.r.eland@gmail.com>
2017-04-11 07:49:57 -0500

    updates to Subscription feature

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-subscription',
@@ -11,30 +11,29 @@ export class SubscriptionComponent {
   expiryMonth: string = '05';
   expiryYear: string = '2018';
   cvc: string = '123';
+  messages: string[] = [];
 
   constructor(private zone: NgZone) { }
 
-  message: string;
-
   getToken() {
-    this.message = 'Loading...';
-
+    this.messages = [];
     (<any>window).Stripe.card.createToken({
       number: this.cardNumber,
       exp_month: this.expiryMonth,
       exp_year: this.expiryYear,
       cvc: this.cvc
-    }, (status: number, response: any) => {
-
-      // Wrapping inside the Angular zone
+    },
+    (status: number, response: any) => {
+      // wrap inside Angular zone in order to catch updates
       this.zone.run(() => {
         if (status === 200) {
-          this.message = 'token: ' + response.card.id;
-        } else {
-          this.message = response.error.message;
+          this.messages.push('token: ' + response.card.id);
+        }
+        else {
+          this.messages.push(response.error.message);
         }
       });
     });
-  }
+  } // getToken
 
-}
+} // class SubscriptionComponent
