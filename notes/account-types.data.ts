bafda08333ee682667be3be2b92916b684e85418
Dave Eland <dave.r.eland@gmail.com>
2017-05-12 07:57:37 -0500

    update notes

--- notes/account-types.data.ts
+++ notes/account-types.data.ts
@@ -10,16 +10,30 @@ export const possibleFieldNames =
 	{monthlyAmount:        {label: 'Monthly Amount',         val:0}},
 	{minimumMonthlyAmount: {label: 'Minimum Monthly Amount', val:0}},
 	{employerContribution: {label: 'Employer Contribution',  val:0}},
-	{growthRate:           {label: 'Growth Rate',            val:0}},
-	{targetAmount:         {label: 'Target Amount',          val:0}},
-	{coverageAmount:       {label: 'Coverage Amount',        val:0}},
-	{maturityDate:         {label: 'Maturity Date',          val:''}}
+	{growthRate:           {label: 'Growth Rate',            val:0}}
+	/*
+	targetAmount          // emergency and general savings
+	maturityDate          // CD
+	coverageAmount        // term, permanent, and group life insurance
+	expirationYear        // term life insurance
+	isPrimaryResidence    // real estate assets
+	isInvestmentProperty  // real estate assets
+	haveMortgage          // real estate assets
+	haveSecondMortgage    // real estate assets
+	haveLien              // real estate assets
+	haveLoanAgainstIRA    // IRA
+	appreciationRate      // other (assets)  (also has Est. Growth Rate)
+	extraYouPay           // credit card
+	creditCardLimit       // credit card
+
+	{isPrimaryResidence: {label: 'Primary Residence', val:true}}
+	*/
 ];
 
 /*
-The following defines all possible account types (the JavaScript attribute name is the name of the type).  Each type is a distinct list of field names that are required for a particular account under a given subaccount.  The presence or absence of a field, and the value of the inclusion attribute are the things that distinguish one type from another.  (An absent field is the same as a field with inclusion N/A.)
+The following defines all possible field types (the JavaScript attribute name is the name of the type).  Each type is a distinct list of field names that are required for a particular account under a given subaccount.  The presence or absence of a field, and the value of the inclusion attribute are the things that distinguish one type from another.  (An absent field is the same as a field with inclusion N/A.)
 */
-export const accountTypes =
+export const fieldTypes =
 {
 	income:
 	[
@@ -48,7 +62,7 @@ export const accountTypes =
 		{minimumMonthlyPayment: {label: 'Minimum Monthly Payment', val:0, inclusion:'optional'}},
 		{rate: {label: 'Rate', val:0, inclusion:'optional'}}
 	]
-	ira:  // individual retirement account [individual retirement plan]
+	retirementAccount:
 	{
 		monthlyAmount: {label: 'monthlyAmount', val:0, inclusion:'required'},
 		accountOwner: {label: 'Account Owner', val:'', inclusion:'optional'},
@@ -59,16 +73,7 @@ export const accountTypes =
 		employerContribution: {label: 'Employer Contribution', val:0, valType:'%', inclusion:'optional'},
 		rate: {label: 'Rate', val:0, inclusion:'required'}
 	},
-	gra:  // group retirement account [employer retirement plan] (is there a better term for this type)
-	{
-		monthlyAmount: {label: 'monthlyAmount', val:0, inclusion:'required'},
-		accountOwner: {label: 'Account Owner', val:'', inclusion:'optional'},
-		description: {label: 'Description', val:'', inclusion:'required'},
-		companyName: {label: 'Company Name', val:'', inclusion:'optional'},
-		accountValue: {label: 'Account Value', val:0, inclusion:'required'},
-		minimumMonthlyPayment: {label: 'Minimum Monthly Payment', val:0, inclusion:'optional'},
-		employerContribution: {label: 'Employer Contribution', val:0, valType:'%', inclusion:'required'},
-		rate: {label: 'Rate', val:0, inclusion:'required'}
+
 	//... others to come
 };  // accountTypes
 
@@ -88,7 +93,7 @@ export const subcategoryAccountTypes =
 			socialSecurity: ['income']
 		}
 	},
-	expense:
+	budget:
 	{
 		giving:
 		{
@@ -104,7 +109,7 @@ export const subcategoryAccountTypes =
 			gas_Electricity: ['expenses']
 		}
 	},
-	asset:
+	assets:
 	{
 
 	}
